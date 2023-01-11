012894733be4a5e8ae6bac23660a7e2f77348e6b
Psyduck <83164321+BotVasu@users.noreply.github.com>
2023-01-11 11:18:13 +0530

    Phase 2: Responsive Button + Responsive Design

--- hamburger.js
+++ hamburger.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+// const gitlogo = document.querySelector(".cta");
 
 
 hamburger.addEventListener("click", () =>{
 
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    // gitlogo.classList.toggle("active");
 })
